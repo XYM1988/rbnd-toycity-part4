40d32386c7aa8415ec40dd6c6078ffa7ea3c4825
Mu <maxmu.phone@gmail.com>
2016-04-19 10:25:35 -0400

    find_by and error is done

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -2,5 +2,17 @@ class Module
   def create_finder_methods(*attributes)
     # Your code goes here!
     # Hint: Remember attr_reader and class_eval
+    attributes.each do |attribute|
+      attr = attribute.to_s
+      self.class_eval("
+        def self.find_by_#{attr}(target_value)
+          self.all.each do |product|
+            if product.#{attr} == target_value
+              return product
+            end
+          end
+        end
+      ")
+    end
   end
 end

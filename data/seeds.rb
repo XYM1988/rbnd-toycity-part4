d5038be22fdffd04c6ea50cb1a33d9167e78c359
Mu <maxmu.phone@gmail.com>
2016-04-19 10:27:11 -0400

    fix a bug

--- data/seeds.rb
+++ data/seeds.rb
@@ -5,4 +5,12 @@ require 'faker'
 
 def db_seed
   # Your code goes here!
-end
+    product_names = []
+    prices = []
+    fake_brands = ['Sony', 'Microsoft', 'Niki', 'Nintendo', 'Google']
+    10.times do
+      Product.create(brand: fake_brands.sample,
+                     name: Faker::Commerce.product_name,
+                     price: Faker::Commerce.price)
+    end
+  end
